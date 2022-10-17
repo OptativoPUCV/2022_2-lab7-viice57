0221880c6d1027767d83f72cd4d58ab3ca76bfaa
Replit user <>
2022-10-17 01:42:17 +0000

    new try

--- heap.c
+++ heap.c
@@ -49,15 +49,16 @@ void heap_push(Heap * pq, void * data, int priority) {
     pq->heapArray[padre].data = aux.data;
 
     if(priority > pq->heapArray[0].priority) {
-    aux.priority = pq->heapArray[hijo].priority;
+        aux.priority = pq->heapArray[hijo].priority;
     aux.data = pq->heapArray[hijo].data;
       
-    pq->heapArray[hijo].priority = pq->heapArray[padre].priority;
-    pq->heapArray[hijo].data = pq->heapArray[padre].data;
+    pq->heapArray[hijo].priority = pq->heapArray[0].priority;
+    pq->heapArray[hijo].data = pq->heapArray[0].data;
       
-    pq->heapArray[padre].priority = aux.priority;
-    pq->heapArray[padre].data = aux.data;
+    pq->heapArray[0].priority = aux.priority;
+    pq->heapArray[0].data = aux.data;
 
+    }
   }
 }
 
