94726e5029cb718d1c4315a90897e184768a946e
Replit user <>
2022-10-17 02:44:39 +0000

    new try

--- heap.c
+++ heap.c
@@ -25,7 +25,10 @@ void * heap_top(Heap * pq){
 
 
 void heap_push(Heap * pq, void * data, int priority) {
-  int hijo = pq->size;
+
+  if(pq->heapArray[pq->size].priority == 0) {
+    int hijo = pq->size;
+  }
   int padre = (hijo - 1) / 2;
   
   if(pq->size == pq->capac) {
